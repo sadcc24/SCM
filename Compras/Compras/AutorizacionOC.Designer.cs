462bd53ad19070622a7c76bcfb018542903d30e7
Jose A Chajon <jchajoncueto@gmail.com>
2017-06-18 10:18:21 -0600

    Pantallas Orden de Compra
    Diseno de las pantallas para ingreso de orden de compra

---
+++ Compras/Compras/AutorizacionOC.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Compras
+{
+    partial class AutorizacionOC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutorizacionOC));
+            this.cboBodega = new System.Windows.Forms.ComboBox();
+            this.lblBodega = new System.Windows.Forms.Label();
+            this.cboEmpresa = new System.Windows.Forms.ComboBox();
+            this.lblEmpresa = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblSede = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.grdDetalleProductos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDetalleProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboBodega
+            // 
+            this.cboBodega.FormattingEnabled = true;
+            this.cboBodega.Location = new System.Drawing.Point(754, 65);
+            this.cboBodega.Name = "cboBodega";
+            this.cboBodega.Size = new System.Drawing.Size(121, 21);
+            this.cboBodega.TabIndex = 13;
+            // 
+            // lblBodega
+            // 
+            this.lblBodega.AutoSize = true;
+            this.lblBodega.Location = new System.Drawing.Point(697, 68);
+            this.lblBodega.Name = "lblBodega";
+            this.lblBodega.Size = new System.Drawing.Size(47, 13);
+            this.lblBodega.TabIndex = 12;
+            this.lblBodega.Text = "Bodega:";
+            // 
+            // cboEmpresa
+            // 
+            this.cboEmpresa.FormattingEnabled = true;
+            this.cboEmpresa.Location = new System.Drawing.Point(493, 65);
+            this.cboEmpresa.Name = "cboEmpresa";
+            this.cboEmpresa.Size = new System.Drawing.Size(121, 21);
+            this.cboEmpresa.TabIndex = 11;
+            // 
+            // lblEmpresa
+            // 
+            this.lblEmpresa.AutoSize = true;
+            this.lblEmpresa.Location = new System.Drawing.Point(436, 68);
+            this.lblEmpresa.Name = "lblEmpresa";
+            this.lblEmpresa.Size = new System.Drawing.Size(51, 13);
+            this.lblEmpresa.TabIndex = 10;
+            this.lblEmpresa.Text = "Empresa:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(224, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // lblSede
+            // 
+            this.lblSede.AutoSize = true;
+            this.lblSede.Location = new System.Drawing.Point(174, 68);
+            this.lblSede.Name = "lblSede";
+            this.lblSede.Size = new System.Drawing.Size(35, 13);
+            this.lblSede.TabIndex = 8;
+            this.lblSede.Text = "Sede:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(353, 25);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(285, 23);
+            this.lblTitulo.TabIndex = 7;
+            this.lblTitulo.Text = "Ingreso de Solicitud Orden Compra";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // grdDetalleProductos
+            // 
+            this.grdDetalleProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDetalleProductos.Location = new System.Drawing.Point(177, 104);
+            this.grdDetalleProductos.Name = "grdDetalleProductos";
+            this.grdDetalleProductos.Size = new System.Drawing.Size(698, 275);
+            this.grdDetalleProductos.TabIndex = 27;
+            // 
+            // AutorizacionOC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 444);
+            this.Controls.Add(this.grdDetalleProductos);
+            this.Controls.Add(this.cboBodega);
+            this.Controls.Add(this.lblBodega);
+            this.Controls.Add(this.cboEmpresa);
+            this.Controls.Add(this.lblEmpresa);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblSede);
+            this.Controls.Add(this.lblTitulo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AutorizacionOC";
+            this.Text = "AutorizacionOC";
+            ((System.ComponentModel.ISupportInitialize)(this.grdDetalleProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboBodega;
+        private System.Windows.Forms.Label lblBodega;
+        private System.Windows.Forms.ComboBox cboEmpresa;
+        private System.Windows.Forms.Label lblEmpresa;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblSede;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.DataGridView grdDetalleProductos;
+    }
+}
