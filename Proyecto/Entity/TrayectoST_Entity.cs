336edea13e864083be423cca4e29567673f5c61b
Pamela Selman <pamelaselman_5@hotmail.com>
2017-07-12 17:07:37 -0600

    Revert "comm"
    This reverts commit 6363078261a00d47a4aaccd64ec36bb0e4ccfb30.

---
+++ Proyecto/Entity/TrayectoST_Entity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class TrayectoST_Entity
+    {
+        public int idSolicitud { get; set; }
+        public int idMovimiento { get; set; }
+        public string fechasalida { get; set; }
+        public string fechallegada { get; set; }
+        public string origen { get; set; }
+        public string destino { get; set; }
+        public decimal kms { get; set; }
+        public int idEmpresa { get; set; }
+        public int idUsuario { get; set; }
+        public int idTrayecto { get; set; }
+        
+        public TrayectoST_Entity() { }
+        public TrayectoST_Entity(int pidSolicitud, int pidMovimiento, string pfechasalida, string pfechallegada, string porigen,
+           string pdestino, decimal pkms, int pidEmpresa, int pidUsuario, int pidTrayecto)
+        {
+            this.idSolicitud = pidSolicitud;
+            this.idMovimiento = pidMovimiento;
+            this.fechallegada = pfechallegada;
+            this.fechasalida = pfechasalida;
+            this.origen = porigen;
+            this.destino = pdestino;
+            this.kms = pkms;
+            this.idEmpresa = pidEmpresa;
+            this.idUsuario = pidUsuario;
+            this.idTrayecto = pidTrayecto;
+            
+        }
+
+
+    }
+}
--- Proyecto/SCM/buscarSolicitudTransporte.cs
+++ Proyecto/SCM/buscarSolicitudTransporte.cs
@@ -21,18 +21,36 @@ namespace SCM
 
         private void btnNuevo_Click(object sender, EventArgs e)
         {
-            ingresoSolicitudTransporte vehiculo = new ingresoSolicitudTransporte(0);
-            vehiculo.MdiParent = this.MdiParent;
-            vehiculo.Show();
+            //ingresoSolicitudTransporte vehiculo = new ingresoSolicitudTransporte(0);
+            ingresoST_Encabezado st = new ingresoST_Encabezado();
+            st.MdiParent = this.MdiParent;
+            st.Show();
+            //vehiculo.MdiParent = this.MdiParent;
+           // vehiculo.Show();
 
         }
 
         #region Funciones y metodos
-        private void ActualizarForm()
+        private void ActualizarForm(int tipo)
         {
+            SolicitudTransporte_Entity st = new SolicitudTransporte_Entity();
+            //string[] empresa = Globales.Empresa.CapturarEmpresa();
+            //string[] usuario = Globales.Usuario.CapturarUsuario();
+            //st.stEmpresa = Convert.ToInt32(empresa[0]);
+            //st.stUsuario = Convert.ToInt32(usuario[0]);
+            st.stEmpresa = 1;
+            st.stUsuario = 1;
+
+            if (tipo == 2)
+            {
+                if (txtSolicit.Text != "")
+                {
+                    st.idMovimiento = Convert.ToInt32(txtSolicit.Text.Trim());
+                }
+            }
             try
             {
-                dgvSolicitud.DataSource = new SolicitudTransporte_BOL().verTodoST(); //N_Cliente().GetAll();
+                dgvSolicitud.DataSource = new SolicitudTransporte_BOL().verTodoST(st);
                 dgvSolicitud.Refresh();
             }
             catch (Exception Ex)
@@ -46,7 +64,7 @@ namespace SCM
 
         private void buscarSolicitudTransporte_Load(object sender, EventArgs e)
         {
-            ActualizarForm();
+            ActualizarForm(1);
         }
 
         private void dgvSolicitud_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
@@ -64,31 +82,33 @@ namespace SCM
             }
 
             DataGridViewRow row = dgvSolicitud.Rows[e.RowIndex];
-            int valor = (int)row.Cells[0].Value;
-            //int codigo = Convert.ToInt32(valor);
-            ingresoSolicitudTransporte ST = new ingresoSolicitudTransporte(valor);
-            ST.MdiParent = this.MdiParent;
-            ST.Show();
+            int idmovimiento = (int)row.Cells[0].Value;
+            string idsolicitud = (string)row.Cells[1].Value;
+            //Verifica si la solicitud ya ha sido creada 
+            if(idsolicitud == "PENDIENTE")
+            {
+                //Pantalla para la creacion de la solicitud
+                ingresoSolicitudTransporte ST = new ingresoSolicitudTransporte(idmovimiento);
+                ST.MdiParent = this.MdiParent;
+                ST.Show();
+            }
+            else
+            {
+                //Pantalla para el seguimiento de la solicitud
+                seguimientoST ST = new seguimientoST(idmovimiento.ToString(), idsolicitud);
+                ST.MdiParent = this.MdiParent;
+                ST.Show();
+            }
         }
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
-            try
-            {
-                SolicitudTransporte_Entity st = new SolicitudTransporte_Entity();
-                st.idSolicitud = Convert.ToInt32(txtSolicit.Text.Trim());
-                dgvSolicitud.DataSource = new SolicitudTransporte_BOL().verST_Single(st);
-                dgvSolicitud.Refresh();
-            }
-            catch (Exception Ex)
-            {
-                MessageBox.Show(Ex.Message);
-            }
+            ActualizarForm(2);
         }
 
         private void btnActualizar_Click(object sender, EventArgs e)
         {
-            ActualizarForm();
+            ActualizarForm(1);
         }
 
         private void txtSolicit_KeyPress(object sender, KeyPressEventArgs e)
@@ -97,6 +117,116 @@ namespace SCM
             {
                 e.Handled = true;
             }   
+        }
+
+        private void btnUltimo_Click(object sender, EventArgs e)
+        {
+            funirUltimo(dgvSolicitud);
+        }
+
+        #region Navegacion
+        //Para ir al primer registro:
+        public static int funirPrimero(DataGridView gridDatos)
+        {
+            if (gridDatos.Rows.Count > 1)
+            {
+                gridDatos.Rows[0].Selected = true;
+                gridDatos.CurrentCell = gridDatos.Rows[0].Cells[0];
+
+                return 0;
+            }
+            else
+            {
+                MessageBox.Show("NO HAY REGISTROS");
+                return 0;
+            }
+
+        }
+        //Para ir al último registro registro:
+        public static int funirUltimo(DataGridView gridDatos)
+        {
+            if (gridDatos.Rows.Count > 1)
+            {
+                int max = gridDatos.Rows.Count - 2;
+                gridDatos.Rows[max].Selected = true;
+                gridDatos.CurrentCell = gridDatos.Rows[max].Cells[0];
+                return 0;
+            }
+            else
+            {
+                MessageBox.Show("NO HAY REGISTROS");
+                return 0;
+            }
+        }
+        //Para ir al registro anterior
+        public static int funirAnterior(DataGridView gridDatos)
+        {
+            if (gridDatos.Rows.Count > 1)
+            {
+                int valor = gridDatos.CurrentCell.RowIndex;
+                int max = gridDatos.Rows.Count - 2;
+                valor = valor - 1;
+                if (valor >= 0)
+                {
+                    gridDatos.Rows[valor].Selected = true;
+                    gridDatos.CurrentCell = gridDatos.Rows[valor].Cells[0];
+                }
+                else { MessageBox.Show("PRIMER REGISTRO"); }
+                return 0;
+            }
+            else
+            {
+                MessageBox.Show("NO HAY DATOS");
+                return 0;
+            }
+        }
+        // para ir al registro siguiente.
+        public static int funirSiguiente(DataGridView gridDatos)
+        {
+            if (gridDatos.Rows.Count > 1)
+            {
+                int valor = gridDatos.CurrentCell.RowIndex;
+                int max = gridDatos.Rows.Count - 2;
+                valor = valor + 1;
+                if (valor <= max)
+                {
+                    gridDatos.Rows[valor].Selected = true;
+                    gridDatos.CurrentCell = gridDatos.Rows[valor].Cells[0];
+                }
+                else { MessageBox.Show("ULTIMO REGISTRO"); }
+                return 0;
+            }
+            else
+            {
+                MessageBox.Show("NO HAY DATOS");
+                return 0;
+            }
+
+        }
+        #endregion
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            funirSiguiente(dgvSolicitud);
+        }
+
+        private void btnAnterior_Click(object sender, EventArgs e)
+        {
+            funirAnterior(dgvSolicitud);
+        }
+
+        private void btnPrimero_Click(object sender, EventArgs e)
+        {
+            funirPrimero(dgvSolicitud);
+        }
+
+        private void btnAyuda_Click(object sender, EventArgs e)
+        {
+            string fullpath = System.IO.Path.Combine(Application.StartupPath, "ManualUsuarioSCM.pdf");
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = fullpath;
+            proc.Start();
+            proc.Close();
         }
     }
 }
